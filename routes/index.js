c2b878d70089fe4a8c4df165ed32230c7d23aa6b
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-02-15 01:02:00 -0500

    starter css for visualization

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,38 @@ router.get("/access-denied", (req, res, next) => {
   res.render("access-denied");
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("employee/login");
+});
+
+router.post("/login", (req, res, next) => {
+  if (!req.body.username) {
+    res.send("You did not include a username");
+  } else if (!req.body.password) {
+    res.send("You need a password");
+  }
+
+  Employee.findOne({ username: req.body.username })
+    .then((foundEmployee) => {
+      if (!foundEmployee) {
+        return res.send("Incorrect username");
+      }
+
+      const match = bcrypt.compareSync(
+        req.body.password,
+        foundEmployee.password
+      );
+
+      if (!match) {
+        return res.send("Incorrect password");
+      }
+
+      req.session.user = foundEmployee;
+      res.render("employee/profile", { employee: req.session.user });
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+});
+
 module.exports = router;
